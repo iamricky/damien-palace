7dd40a52e114767a5f76abf7fb7254585a45f335
heynicecode <rickymuldrew@gmail.com>
2015-03-24 02:53:18 -0400

    create header and footer files

---
+++ footer.php
@@ -0,0 +1,15 @@
+            <footer>
+                <h1>Follow Damien</h1>
+                <ul>
+                    <li>facebook</li>
+                    <li>instagram</li>
+                    <li>twitter</li>
+                    <li>youtube</li>
+                </ul>
+            </footer>
+        </div> <!-- end .container-fluid -->
+
+        <!-- Javcascript -->
+        <?php wp_footer(); ?>
+    </body>
+</html>
