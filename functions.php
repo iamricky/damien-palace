98948e01e05872511dfdaebe6f6e6413e0ac881c
heynicecode <rickymuldrew@gmail.com>
2015-03-25 10:02:30 -0400

    update paths

--- functions.php
+++ functions.php
@@ -3,15 +3,14 @@
 
 add_action( "wp_enqueue_scripts", "wp_styles_scripts" );
 function wp_styles_scripts() {
-  $template_url = get_template_directory_uri();
-  $css_dir = "/assets/css/";
-  $js_dir = "/assets/js/";
+  $temp_dir   = get_template_directory_uri();
+  $css_dir    = "/assets/css/";
+  $js_dir     = "/assets/js/";
 
   global $wp_scripts;
 
   //CSS
-  wp_enqueue_style( "master-stylesheet"
-    , get_template_directory_uri() . $css_dir . "screen.css" );
+  wp_enqueue_style( "master-stylesheet", $temp_dir . $css_dir . "screen.css" );
 
   //Javascript
 }
