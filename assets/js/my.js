2a1eb3e30f05286c5bd7509139eda078fb5de25b
heynicecode <rickymuldrew@gmail.com>
2015-06-02 19:58:13 -0400

    refactor ajax js: - move $.ajax to function - create array for promises - add $.when to handle resolved promises

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,27 +1,19 @@
-jQuery(document).ready(function($) {
+jQuery(function($) {
 
-    var initPhotos = $.ajax({
-        url: the_api.ajax_url,
-        type: "post",
-        dataType: "json",
-        data: {
-            action: "query_photos"
-        }
-    });
+    var fetchMedia = function(media) {
+            return $.ajax({
+                dataType: "json",
+                data: {
+                    action: "query_" + media
+                },
+                type: "post",
+                url: the_api.ajax_url
+            });
+        },
+        requests = [
+            fetchMedia("photos"),
+            fetchMedia("videos")
+        ];
 
-    initPhotos.done(function(data) {
-        console.log("got photos!");
-
-        $.each(data, function(index, obj) {
-
-            var $elem = $(".photos").find(".row:last"),
-                output = "<div class='photo'>";
-
-                output += "<img class='img-sm' src=" + obj.small_img + " />",
-                output += "<img class='img-lg' src=" + obj.large_img + " />",
-                output += "</div>";
-
-            $elem.append(output);
-        });
-    });
+    $.when.apply($, requests).then(function(photos, videos) {});
 });
