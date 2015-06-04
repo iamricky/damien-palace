d6d4267ac436462d8e642dba6d3a4e16c8f3a2e6
heynicecode <rickymuldrew@gmail.com>
2015-06-04 15:41:56 -0400

    - create fetchView func to find views - create fetchPartial func to return partial dir - create setupView to $.get partials - refactor data structures for views and api data

--- assets/js/my.js
+++ assets/js/my.js
@@ -7,13 +7,74 @@ jQuery(function($) {
                     action: "query_" + media
                 },
                 type: "post",
-                url: the_api.ajax_url
+                url: wp_urls.ajax_url
             });
         },
-        requests = [
-            fetchMedia("photos"),
-            fetchMedia("videos")
-        ];
+        fetchView = function(className) {
+            return $(className).find(".row:last");
+        },
+        fetchPartial = function(partialName) {
+            var dir = wp_urls.template_url + "/assets/js/partials/",
+                partial = dir + partialName;
 
-    $.when.apply($, requests).then(function(photos, videos) {});
+            return partial;
+        },
+        setupView = function(partial, view, data) {
+            $.get(fetchPartial(partial), function(template) {
+
+                var $view = view,
+                    videos = data;
+
+                $.each(videos, function(idx, obj) {
+
+                    var rendered = Mustache.render(template, obj);
+
+                    if (idx < 8) {
+                        $view.append(rendered);
+                    } else {
+                        return false
+                    }
+                });
+            });
+
+        }
+
+    var data = {},
+        view = {
+            photo: fetchView(".photos"),
+            video: {
+                official: fetchView(".videos"),
+                munzmind: fetchView(".munz-mind")
+            }
+        }
+
+    var requests = [
+        fetchMedia("photos"),
+        fetchMedia("videos")
+    ];
+
+    $.when.apply($, requests).then(function(photos, videos) {
+
+        data = {
+            photo: photos[0],
+            video: videos[0]
+        },
+        setupParams = [{
+            partial: "photos.mst",
+            view: view.photo,
+            data: data.photo
+        }, {
+            partial: "official.mst",
+            view: view.video.official,
+            data: data.video.official
+        }, {
+            partial: "munzmind.mst",
+            view: view.video.munzmind,
+            data: data.video.munzmind
+        }]
+
+        $.each(setupParams, function(idx, obj) {
+            setupView(obj.partial, obj.view, obj.data);
+        });
+    });
 });
