4bcd9003e3538999610d2795697f421413f7dc90
heynicecode <rickymuldrew@gmail.com>
2015-06-19 20:24:57 -0400

    - add theme support for thumbnails - create custom post types for albums and events

--- functions.php
+++ functions.php
@@ -7,6 +7,10 @@ include_once "api/youtube.php";
 $instagram = new InstagramApi( $keys["instagram"] );
 $youtube = new YouTubeApi( $keys["youtube"] );
 
+add_theme_support( "post-thumbnails" );
+add_image_size( "music-thumb", 350, 348 );
+add_image_size( "news-thumb", 350, 360 );
+
 add_action( "wp_enqueue_scripts", "enqueue_assets" );
 function enqueue_assets() {
   $temp_dir   = get_template_directory_uri();
@@ -37,6 +41,74 @@ function enqueue_assets() {
     )
   );
 }
+
+add_action( "init", "enqueue_post_types" );
+function enqueue_post_types() {
+
+  $labels = array(
+    "name"               => "Music",
+    "singular_name"      => "Music",
+    "menu_name"          => "Music",
+    "name_admin_bar"     => "Music",
+    "add_new"            => "Add New Album",
+    "add_new_item"       => "Add New Album",
+    "new_item"           => "New Album",
+    "edit_item"          => "Edit Album",
+    "view_item"          => "View Album",
+    "all_items"          => "All Albums",
+    "parent_item_colon"  => "Parent Album:",
+    "not_found"          => "No albums found.",
+    "not_found_in_trash" => "No albums found in Trash."
+  );
+  $args = array(
+    "labels"             => $labels,
+    "public"             => true,
+    "publicly_queryable" => true,
+    "show_ui"            => true,
+    "show_in_nav_menus"  => false,
+    "menu_position"      => 5,
+    "menu_icon"          => "dashicons-format-audio",
+    "query_var"          => true,
+    "rewrite"            => array( "slug" => "music" ),
+    "capability_type"    => "post",
+    "has_archive"        => false,
+    "supports"           => array( "title", "thumbnail", "excerpt" )
+  );
+  register_post_type( "music", $args );
+
+  $labels = array(
+    "name"               => "Events",
+    "singular_name"      => "Event",
+    "menu_name"          => "Events",
+    "name_admin_bar"     => "Event",
+    "add_new"            => "Add New Event",
+    "add_new_item"       => "Add New Event",
+    "new_item"           => "New Event",
+    "edit_item"          => "Edit Event",
+    "view_item"          => "View Event",
+    "all_items"          => "All Events",
+    "parent_item_colon"  => "Parent Events:",
+    "not_found"          => "No events found.",
+    "not_found_in_trash" => "No events found in Trash."
+  );
+  $args = array(
+    "labels"              => $labels,
+    "public"              => true,
+    "publicly_queryable"  => true,
+    "show_ui"             => true,
+    "show_in_nav_menus"   => false,
+    "menu_position"       => 5,
+    "menu_icon"           => "dashicons-calendar-alt",
+    "query_var"           => true,
+    "rewrite"             => array( "slug" => "events" ),
+    "capability_type"     => "post",
+    "has_archive"         => false,
+    "supports"            => array( "title" )
+  );
+  register_post_type( "events", $args );
+  flush_rewrite_rules();
+}
+
 add_action( "wp_ajax_nopriv_query_photos", "my_photo_query" );
 add_action( "wp_ajax_query_photos", "my_photo_query" );
 function my_photo_query() {
@@ -69,4 +141,6 @@ function my_video_query() {
   echo $data;
 
   wp_die();
-} ?>
+}
+
+?>
